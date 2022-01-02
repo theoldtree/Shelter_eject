a495da5414c79b837285d3cc9efb1d1aabd2e133
유정무 <yujeongmu@yujeongmuui-MacBookPro.local>
2022-01-02 20:07:31 +0900

    install dependencies and upoad required images

---
+++ android/app/build/generated/source/buildConfig/debug/com/theoldtree/shelter_eject/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.theoldtree.shelter_eject;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.theoldtree.shelter_eject";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
